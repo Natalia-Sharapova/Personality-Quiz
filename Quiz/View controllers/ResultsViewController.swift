52b2c4467bce5231da940f6303315931512bcf90
Natalia Sharapova <emuradyants2005@gmail.com>
2021-09-14 20:02:16 +0300

    Finished UI

---
+++ Quiz/View controllers/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  Quiz
+//
+//  Created by Наталья Шарапова on 14.09.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
