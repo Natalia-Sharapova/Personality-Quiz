8e41430de3dd6f3d7be22ae9256987010f283885
Natalia Sharapova <emuradyants2005@gmail.com>
2021-09-18 20:45:41 +0300

    Finished project

--- Quiz/View controllers/QuestionViewController.swift
+++ Quiz/View controllers/QuestionViewController.swift
@@ -8,22 +8,134 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multiplyStackView: UIStackView!
+    @IBOutlet var multiLabel: [UILabel]!
+    @IBOutlet var multiSwitch: [UISwitch]!
+    
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet var rangeLabel: [UILabel]!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    var answersChosen: [Answer] = [] {
+    didSet {
+        print(#line, #function, answersChosen)
+    }
+    }
+    var currentAnswers: [Answer] {
+        currentQuestion.answers
+    }
+    
+    var currentQuestion: Question {
+        Question.all[questionIndex]
+    }
+    
+    var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        rangedSlider.maximumValue = 0.99999
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI () {
+        
+        func updateSingleStack () {
+            singleStackView.isHidden = false
+            for (index, button) in singleButtons.enumerated() {
+                button.setTitle(nil, for: [])
+                button.tag = index
+            }
+            for (button, answer) in zip(singleButtons, currentAnswers) {
+                button.setTitle(answer.text, for: [])
+            }
     }
-    */
+        
+        func updateMultiplyStack () {
+            multiplyStackView.isHidden = false
+            for label in multiLabel {
+                label.text = nil
+            }
+            for (label, answer) in zip(multiLabel, currentAnswers) {
+                label.text = answer.text
+    }
+        }
+        
+        func updateRangeStack () {
+            rangedStackView.isHidden = false
+            rangeLabel.first?.text = currentAnswers.first?.text
+            rangeLabel.last?.text = currentAnswers.last?.text
+    }
+        
+        for stackView in [singleStackView, multiplyStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        let currentQuestion = Question.all[questionIndex]
+        let totalProgress = Float(questionIndex) / Float(Question.all.count)
+        
+        navigationItem.title = "Вопрос № \(questionIndex + 1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack()
+        case .multiply:
+          updateMultiplyStack()
+        case .range:
+           updateRangeStack()
+        }
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        if questionIndex < Question.all.count {
+        updateUI()
+        } else {
+        performSegue(withIdentifier: "Results segue", sender: nil)
+    }
+    }
 
+    @IBAction func singleButtonPressed(_ sender: UIButton) {
+        let answers = Question.all[questionIndex].answers
+        let index = sender.tag
+        guard  0 <= index && index < answers.count else {
+            return
+        }
+        let answer = answers[index]
+        answersChosen.append(answer)
+       nextQuestion()
+    }
+    
+    @IBAction func multiButtonPressed () {
+        print(#line, #function)
+        for (index, multiSwitch) in multiSwitch.enumerated(){
+            if multiSwitch.isOn && index < currentAnswers.count {
+                let answer = currentAnswers[index]
+                answersChosen.append(answer)
+    }
+}
+        nextQuestion()
+    }
+    
+    @IBAction func rangeButtonPressed () {
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        if index < currentAnswers.count {
+        let answer = currentAnswers[index]
+        answersChosen.append(answer)
+        }
+        nextQuestion()
+    }
+    
+    @IBSegueAction func resultsSegue(_ coder: NSCoder) -> ResultsViewController? {
+        return ResultsViewController(coder: coder, answers: answersChosen)
+    }
 }
--- Quiz/View controllers/ResultsViewController.swift
+++ Quiz/View controllers/ResultsViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    let answers: [Answer]
+    
+    init? (coder: NSCoder, answers: [Answer]) {
+        self.answers = answers
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("#line, #function init(coder:) has not been implemented")
+    }
+    
+    func calculatePersonalityResult () {
+        let frequencyOfAnswers = answers.reduce(into: [:]) { counts, answer in
+            counts[answer.type, default: 0] += 1
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { pair1, pair2 in
+            pair1.value > pair2.value }.first!.key
+        updateUI(with: mostCommonAnswer)
+        
+        print(#line, #function, frequencyOfAnswers)
+        }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
     }
-
-
+    func updateUI (with animal: AnimalType) {
+        animalLabel.text = "Вы - \(animal.rawValue)"
+        descriptionLabel.text = animal.definition
+}
 }
